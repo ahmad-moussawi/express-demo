02e06bd91ae2e3a4c0b6ca29778a7f0172b5daf1
Ahmad Moussawi <aymoussawi@gmail.com>
2019-01-22 15:07:46 +0200

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+router.get('/text', function(req, res, next) {
+  res.send('this is the index router');
+});
+
+router.get('/hello/:name', function(req, res, next) {
+  var name = req.params.name;
+  res.send(`<div>Hello <b>${name}</b></div>`);
+});
+
+router.post('/save', function(req, res) {
+
+  var data = req.body;
+
+  data.all = data.user + ':' + data.password;
+
+  // todo save the data in the database
+
+  res.send(data);
+
+});
+
+
+
+module.exports = router;
